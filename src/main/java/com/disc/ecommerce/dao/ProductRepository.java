502e9a888737fed3763b29b75775a8cc0c149ba1
MatildaKällström <matilda.kallstrom@hotmail.se>
2023-05-10 09:00:36 +0200

    DB and pictures

--- src/main/java/com/disc/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/disc/ecommerce/dao/ProductRepository.java
@@ -4,13 +4,14 @@ import com.disc.ecommerce.entity.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
+//import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 
-   Page<Product> findByNameContaining(@Param("name") String name, Pageable page);
+   Page<Product> findByNameContaining(@RequestParam("name") String name, Pageable pageable);
 
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -13,27 +13,99 @@ INSERT INTO product_category(category_name) VALUES ('Glow');
 
 INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
 unit_price, category_id, date_created)
-VALUES ('DISC-TECH-1000', 'Pure', 'Hippo 3 3 -1 -1',
-'assets/images/products/putt/putt.png'
-,1,100,249,1, NOW());
+VALUES ('DISC-TECH-1000', 'Pure', 'Ice `n Ice 3 / 3 / -1 / 1',
+'assets/images/products/putt/pure.png'
+,1,1,400,1, NOW());
 
 INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
 unit_price, category_id, date_created)
-VALUES ('DISC-TECH-1001', 'Mercy', 'Hippo 3 3 -1 -1',
-'assets/images/products/putt/putt.png'
-,1,100,249,1, NOW());
+VALUES ('DISC-TECH-1001', 'Mercy', 'Grey 2 / 4 / 0 / 1',
+'assets/images/products/putt/mercy.png'
+,1,1,400,1, NOW());
 
 INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
 unit_price, category_id, date_created)
-VALUES ('DISC-TECH-1002', 'Ruby', 'Hippo 3 3 -1 -1',
+VALUES ('DISC-TECH-1002', 'Pure', 'Hippo 3 / 3 / -1 / -1',
 'assets/images/products/putt/putt.png'
-,1,100,249,1, NOW());
+,1,1,400,1, NOW());
 
 INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
 unit_price, category_id, date_created)
-VALUES ('DISC-TECH-1003', 'Reko', 'Hippo 3 3 -1 -1',
+VALUES ('DISC-TECH-1003', 'Reko', 'Rose and Skull 3 / 3 / 0 / 2',
+'assets/images/products/putt/reko.png'
+,1,1,400,1, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
+unit_price, category_id, date_created)
+VALUES ('DISC-TECH-1004', 'Pure', 'Ice n Ice 3 / 3 / -1 / 1',
+'assets/images/products/putt/pure.png'
+,1,1,400,1, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
+unit_price, category_id, date_created)
+VALUES ('DISC-TECH-1005', 'Reko', 'Rose and Skull 3 / 3 / 0 / 2',
+'assets/images/products/putt/reko.png'
+,1,1,400,1, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
+unit_price, category_id, date_created)
+VALUES ('DISC-TECH-1006', 'Westside disc Harp', 'Swirl Green and Purple 4 / 3 / 0 / 3',
+'assets/images/products/putt/harp.png'
+,1,1,400,1, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
+unit_price, category_id, date_created)
+VALUES ('DISC-TECH-1007', 'Westside disc Harp', 'Fire n Ice 4 / 3 / 0 / 3',
+'assets/images/products/putt/harp-fire.png'
+,1,1,400,1, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
+unit_price, category_id, date_created)
+VALUES ('DISC-TECH-1008', 'Keystone', 'Fire n Ice 2 / 5 / -1 / 1',
+'assets/images/products/putt/keystone-fire.png'
+,1,1,400,1, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
+unit_price, category_id, date_created)
+VALUES ('DISC-TECH-1009', 'Keystone', 'Fire n Ice 2 / 5 / -1 / 1',
+'assets/images/products/putt/keystone-fire.png'
+,1,1,400,1, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
+unit_price, category_id, date_created)
+VALUES ('DISC-TECH-1010', 'Westside disc Harp', 'Swirl Green and Purple 4 / 3 / 0 / 3',
+'assets/images/products/putt/harp.png'
+,1,1,400,1, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
+unit_price, category_id, date_created)
+VALUES ('DISC-TECH-1011', 'Westside disc Harp', 'Fire n Ice 4 / 3 / 0 / 3',
+'assets/images/products/putt/harp-fire.png'
+,1,1,400,1, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
+unit_price, category_id, date_created)
+VALUES ('DISC-TECH-1012', 'Järn', 'Kastaplast, Purple 4.5 / 3 / 0 / 3',
+'assets/images/products/putt/jarn.png'
+,1,1,400,1, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
+unit_price, category_id, date_created)
+VALUES ('DISC-TECH-1013', 'Järn', 'Kastaplast, Purple 4.5 / 3 / 0 / 3',
+'assets/images/products/putt/jarn.png'
+,1,1,400,1, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
+unit_price, category_id, date_created)
+VALUES ('DISC-TECH-1014', 'Mercy', 'Grey 2 / 4 / 0 / 1',
+'assets/images/products/putt/mercy.png'
+,1,1,400,1, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
+unit_price, category_id, date_created)
+VALUES ('DISC-TECH-1015', 'Pure', 'Hippo 3 / 3 / -1 / -1',
 'assets/images/products/putt/putt.png'
-,1,100,249,1, NOW());
+,1,1,400,1, NOW());
 
 -- -----------------------------------------------------
 -- Midrange
@@ -41,39 +113,99 @@ VALUES ('DISC-TECH-1003', 'Reko', 'Hippo 3 3 -1 -1',
 
 INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
 unit_price, category_id, date_created)
-VALUES ('DISC-TECH-1000', 'Scooby doo', 'Scooby doo 6 6 4 3',
-'assets/images/products/midrange/midrange-1000.png'
-,1,100,249,2, NOW());
+VALUES ('DISC-TECH-1000', 'River', 'River 7 / 7 / -1 / 1',
+'assets/images/products/midrange/river.png'
+,1,1,400,2, NOW());
 
 INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
 unit_price, category_id, date_created)
-VALUES ('DISC-TECH-1001', 'Fuse', 'Scooby doo 6 6 4 3',
+VALUES ('DISC-TECH-1001', 'Fuse', 'Scooby doo 6 / 6 / 4 / 3',
 'assets/images/products/midrange/midrange-1000.png'
-,1,100,249,2, NOW());
+,1,1,500,2, NOW());
 
 INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
 unit_price, category_id, date_created)
-VALUES ('DISC-TECH-1002', 'Claymore', 'Scooby doo 6 6 4 3',
-'assets/images/products/midrange/midrange-1000.png'
-,1,100,249,2, NOW());
+VALUES ('DISC-TECH-1002', 'Claymore', 'Wood 6 / 6 / 4 / 3',
+'assets/images/products/midrange/claymore.png'
+,1,1,500,2, NOW());
 
 INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
 unit_price, category_id, date_created)
-VALUES ('DISC-TECH-1003', 'Pearl', 'Scooby doo 6 6 4 3',
-'assets/images/products/midrange/midrange-1000.png'
-,1,100,249,2, NOW());
+VALUES ('DISC-TECH-1003', 'Compass', 'Latitude 64, Fire n Ice 5 / 5 / 0 / 1',
+'assets/images/products/midrange/compass-fire.png'
+,1,1,400,2, NOW());
 
 INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
 unit_price, category_id, date_created)
-VALUES ('DISC-TECH-1004', 'Pop Corn', 'Scooby doo 6 6 4 3',
-'assets/images/products/midrange/midrange-1000.png'
-,1,100,249,2, NOW());
+VALUES ('DISC-TECH-1004', 'Compass', 'Latitude 64, Fire n Ice 5 / 5 / 0 / 1',
+'assets/images/products/midrange/compass-fire.png'
+,1,1,400,2, NOW());
 
 INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
 unit_price, category_id, date_created)
-VALUES ('DISC-TECH-1005', 'Buzzz', 'Scooby doo 6 6 4 3',
+VALUES ('DISC-TECH-1005', 'Compass', 'Latitude 64, Eye 5 / 5 / 0 / 1',
+'assets/images/products/midrange/compass-eye.png'
+,1,1,400,2, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
+unit_price, category_id, date_created)
+VALUES ('DISC-TECH-1006', 'Diamond', 'Diamond 6 / 6 / 4 / 3',
+'assets/images/products/midrange/diamond.png'
+,1,1,400,2, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
+unit_price, category_id, date_created)
+VALUES ('DISC-TECH-1007', 'Compass', 'Latitude 64, Eye 5 / 5 / 0 / 1',
+'assets/images/products/midrange/compass-eye.png'
+,1,1,400,2, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
+unit_price, category_id, date_created)
+VALUES ('DISC-TECH-1008', 'Claymore', 'Wood 6 / 6 / 4 / 3',
+'assets/images/products/midrange/claymore.png'
+,1,1,400,2, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
+unit_price, category_id, date_created)
+VALUES ('DISC-TECH-1009', 'River', 'River 6 6 4 3',
+'assets/images/products/midrange/river.png'
+,1,1,400,2, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
+unit_price, category_id, date_created)
+VALUES ('DISC-TECH-1010', 'Göte', 'Göte 4/5/0/1',
+'assets/images/products/midrange/gote.png'
+,1,1,400,2, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
+unit_price, category_id, date_created)
+VALUES ('DISC-TECH-1011', 'Göte', 'Göte 4/5/0/1',
+'assets/images/products/midrange/gote.png'
+,1,1,400,2, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
+unit_price, category_id, date_created)
+VALUES ('DISC-TECH-1012', 'Fuse', 'Scooby doo 6 / 6 / 4 / 3',
 'assets/images/products/midrange/midrange-1000.png'
-,1,100,249,2, NOW());
+,1,1,400,2, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
+unit_price, category_id, date_created)
+VALUES ('DISC-TECH-1012', 'Diamond', 'Diamond 6 / 6 / 4 / 3',
+'assets/images/products/midrange/diamond.png'
+,1,1,400,2, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
+unit_price, category_id, date_created)
+VALUES ('DISC-TECH-1013', 'Saint', 'Skull 9 / 7 / -1 / 2',
+'assets/images/products/midrange/midrange-1001.png'
+,1,1,400,2, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
+unit_price, category_id, date_created)
+VALUES ('DISC-TECH-1014', 'Saint', 'Skull 9 / 7 / -1 / 2',
+'assets/images/products/midrange/midrange-1001.png'
+,1,1,400,2, NOW());
 
 -- -----------------------------------------------------
 -- Distance Driver
@@ -81,39 +213,99 @@ VALUES ('DISC-TECH-1005', 'Buzzz', 'Scooby doo 6 6 4 3',
 
 INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
 unit_price, category_id, date_created)
-VALUES ('DISC-TECH-1000', 'Latitud', 'Elefant 7 4 3 2',
-'assets/images/products/distancedriver/ddriver-1000.png'
-,1,100,199,3, NOW());
-
-INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
-unit_price, category_id, date_created)
-VALUES ('DISC-TECH-1001', 'Explorer', 'Explorer 9 5 5 2',
+VALUES ('DISC-TECH-1000', 'Latitud', 'Elefant 7 / 4 / 3 / 2',
 'assets/images/products/distancedriver/ddrive-1001.png'
-,1,100,299,3, NOW());
+,1,1,400,3, NOW());
 
 INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
 unit_price, category_id, date_created)
-VALUES ('DISC-TECH-1002', 'Sapphire', 'Elefant 7 4 3 2',
+VALUES ('DISC-TECH-1001', 'Explorer', 'Explorer 9 / 5 / 5 / 2',
+'assets/images/products/distancedriver/explorer.png'
+,1,1,500,3, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
+unit_price, category_id, date_created)
+VALUES ('DISC-TECH-1002', 'Mumin', 'Mumin 7 / 4 / 3 / 2',
+'assets/images/products/distancedriver/mumin.png'
+,1,1,300,3, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
+unit_price, category_id, date_created)
+VALUES ('DISC-TECH-1003', 'Explorer', 'Explorer 9 / 5 / 5 / 2',
+'assets/images/products/distancedriver/explorer.png'
+,1,1,500,3, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
+unit_price, category_id, date_created)
+VALUES ('DISC-TECH-1004', 'Raptor', 'Raptor 7 / 4 / 3 / 2',
+'assets/images/products/distancedriver/raptor.png'
+,1,1,400,3, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
+unit_price, category_id, date_created)
+VALUES ('DISC-TECH-1005', 'Raptor', 'Raptor 9 / 5 / 5 / 2',
+'assets/images/products/distancedriver/raptor1.png'
+,1,1,400,3, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
+unit_price, category_id, date_created)
+VALUES ('DISC-TECH-1006', 'Raptor', 'Explorer 9 / 5 / 5 / 2',
+'assets/images/products/distancedriver/raptor.png'
+,1,1,400,3, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
+unit_price, category_id, date_created)
+VALUES ('DISC-TECH-1007', 'Sergant', 'Sergant 9 / 5 / 5 / 2',
+'assets/images/products/distancedriver/sergant.png'
+,1,1,400,3, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
+unit_price, category_id, date_created)
+VALUES ('DISC-TECH-1008', 'Explorer', 'Explorer 9 / 5 / 5 / 2',
 'assets/images/products/distancedriver/ddriver-1000.png'
-,1,100,199,3, NOW());
+,1,1,400,3, NOW());
 
 INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
 unit_price, category_id, date_created)
-VALUES ('DISC-TECH-1003', 'Ballista', 'Explorer 9 5 5 2',
+VALUES ('DISC-TECH-1009', 'Mumin', 'Mumin 9 / 5 / 5 / 2',
+'assets/images/products/distancedriver/mumin.png'
+,1,1,400,3, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
+unit_price, category_id, date_created)
+VALUES ('DISC-TECH-1010', 'Latitud', 'Elefant 7 / 4 / 3 / 2',
 'assets/images/products/distancedriver/ddrive-1001.png'
-,1,100,299,3, NOW());
+,1,1,400,3, NOW());
 
 INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
 unit_price, category_id, date_created)
-VALUES ('DISC-TECH-1004', 'Astronaut', 'Elefant 7 4 3 2',
+VALUES ('DISC-TECH-1011', 'Raptor', 'Raptor 9 / 5 / 5 / 2',
+'assets/images/products/distancedriver/raptor1.png'
+,1,1,400,3, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
+unit_price, category_id, date_created)
+VALUES ('DISC-TECH-1012', 'Explorer', 'Explorer 9 / 5 / 5 / 2',
 'assets/images/products/distancedriver/ddriver-1000.png'
-,1,100,199,3, NOW());
+,1,1,400,3, NOW());
 
 INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
 unit_price, category_id, date_created)
-VALUES ('DISC-TECH-1005', 'Raptor', 'Explorer 9 5 5 2',
-'assets/images/products/distancedriver/ddrive-1001.png'
-,1,100,299,3, NOW());
+VALUES ('DISC-TECH-1013', 'Sergant', 'Sergant 9 / 5 / 5 / 2',
+'assets/images/products/distancedriver/sergant.png'
+,1,1,400,3, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
+unit_price, category_id, date_created)
+VALUES ('DISC-TECH-1013', 'Musket', 'Wood 9 / 5 / 5 / 2',
+'assets/images/products/distancedriver/musket-wood.png'
+,1,1,400,3, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
+unit_price, category_id, date_created)
+VALUES ('DISC-TECH-1013', 'Musket', 'Wood 9 / 5 / 5 / 2',
+'assets/images/products/distancedriver/musket.png'
+,1,1,400,3, NOW());
 
 -- -----------------------------------------------------
 -- Glow
@@ -121,38 +313,38 @@ VALUES ('DISC-TECH-1005', 'Raptor', 'Explorer 9 5 5 2',
 
 INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
 unit_price, category_id, date_created)
-VALUES ('DISC-TECH-1000', 'Smurf', 'Smurf 4 5 3 4',
+VALUES ('DISC-TECH-1000', 'Smurf', 'Smurf 4 / 5 / 3 / 4',
 'assets/images/products/glow/glow-1000.png'
-,1,100,299,4, NOW());
+,1,1,400,4, NOW());
 
 INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
 unit_price, category_id, date_created)
-VALUES ('DISC-TECH-1001', 'Rainmaker', 'Smurf 4 5 3 4',
-'assets/images/products/glow/glow-1000.png'
-,1,100,299,4, NOW());
+VALUES ('DISC-TECH-1001', 'Rainmaker', 'Northern Lights 4 / 5 / 3 / 4',
+'assets/images/products/glow/glow-1001.png'
+,1,1,400,4, NOW());
 
 INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
 unit_price, category_id, date_created)
-VALUES ('DISC-TECH-1002', 'RocX3', 'Smurf 4 5 3 4',
+VALUES ('DISC-TECH-1002', 'RocX3', 'Smurf 4 / 5 / 3 / 4',
 'assets/images/products/glow/glow-1000.png'
-,1,100,299,4, NOW());
+,1,1,300,4, NOW());
 
 INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
 unit_price, category_id, date_created)
-VALUES ('DISC-TECH-1003', 'Leopard3', 'Smurf 4 5 3 4',
-'assets/images/products/glow/glow-1000.png'
-,1,100,299,4, NOW());
+VALUES ('DISC-TECH-1003', 'Leopard3', 'Northern Lights 4 / 5 / 3 / 4',
+'assets/images/products/glow/glow-1001.png'
+,1,1,400,4, NOW());
 
 INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
 unit_price, category_id, date_created)
-VALUES ('DISC-TECH-1004', 'Reko X', 'Smurf 4 5 3 4',
+VALUES ('DISC-TECH-1004', 'Reko X', 'Smurf 4 / 5 / 3 / 4',
 'assets/images/products/glow/glow-1000.png'
-,1,100,299,4, NOW());
+,1,1,500,4, NOW());
 
 INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
 unit_price, category_id, date_created)
-VALUES ('DISC-TECH-1005', 'Grym X', 'Smurf 4 5 3 4',
-'assets/images/products/glow/glow-1000.png'
-,1,100,299,4, NOW());
+VALUES ('DISC-TECH-1005', 'Grym X', 'Northern Lights 4 / 5 / 3 / 4',
+'assets/images/products/glow/glow-1001.png'
+,1,1,400,4, NOW());
 
 
