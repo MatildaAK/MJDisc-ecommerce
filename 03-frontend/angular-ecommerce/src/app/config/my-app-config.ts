abbddde9a064c57e17113b12fb26ac191063fd20
MatildaKällström <matilda.kallstrom@hotmail.se>
2023-06-02 17:18:30 +0200

    Security - Login/Logout

---
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oa9szfx737sMJigv5d7',
+        issuer: 'https://dev-66161579.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
