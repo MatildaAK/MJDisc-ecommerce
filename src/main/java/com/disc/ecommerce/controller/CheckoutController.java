332c84a994d45975b9b64fa8b19b70a9693c83e5
MatildaAK <matilda.kallstrom@hotmail.se>
2023-07-21 16:30:00 +0200

    Stripe Backend

--- src/main/java/com/disc/ecommerce/controller/CheckoutController.java
+++ src/main/java/com/disc/ecommerce/controller/CheckoutController.java
@@ -10,10 +10,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.logging.Logger;
+
 @RestController
 @RequestMapping("/api/checkout")
 public class CheckoutController {
 
+
+    private Logger logger = Logger.getLogger(getClass().getName());
     private CheckoutService checkoutService;
 
     public CheckoutController(CheckoutService checkoutService) {
@@ -30,6 +34,8 @@ public class CheckoutController {
 
     @PostMapping("/payment-intent")
     public ResponseEntity<String> createPaymentIntent(@RequestBody PaymentInfo paymentInfo) throws StripeException {
+
+        logger.info("paymentInfo.amount: " + paymentInfo.getAmount());
 
         PaymentIntent paymentIntent = checkoutService.createPaymentIntent(paymentInfo);
 
